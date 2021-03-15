312024ed6f935b339de2f9d1395281d4d2ace8e2
Emmanuel Adiba <eabaagah@gmail.com>
2021-03-15 17:48:13 +0000

    update

--- database/migrations/2021_03_15_164848_create_items_table.php
+++ database/migrations/2021_03_15_164848_create_items_table.php
@@ -15,6 +15,10 @@ class CreateItemsTable extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->integer('price');
+            $table->text('description');
+            $table->foreignId('category_id');
             $table->timestamps();
         });
     }
