3e3f76e520c87296e4e5300971212cb6073f1041
Emmanuel Adiba <eabaagah@gmail.com>
2021-03-15 17:57:55 +0000

    item model

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Item extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'price',
+        'description',
+        'category_id'
+    ];
+
+    public function category() {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
 }
